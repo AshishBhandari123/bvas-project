79dfb06ae7798d438f1a6af8bebdacffb9e217d5
Ashish Bhandari <officialashish555@gmail.com>
2026-01-18 23:26:28 +0530

    uodfrontend url in cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bvas-project.onrender.com"],
+    origin: ["http://localhost:3000", "https://bvas-beta.vercel.app"],
     credentials: true,
   }),
 );
